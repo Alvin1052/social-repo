77c4b81fc898cbbfd581df12d3622de604266384
alvin1052 <alvin10821@gmail.com>
2025-07-02 11:33:17 +0700

    delete route

--- src/app/api/posts/[id]/route.ts
+++
@@ -1,5 +0,0 @@
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get('id');
-  // atau gunakan NextRequest jika pakai next/server
-}
